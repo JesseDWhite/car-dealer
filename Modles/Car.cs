756ef13ae7e726f0ce575889b5c1b17f61098a70
Jesse White <jesse.white6@gmail.com>
2021-04-30 17:04:16 -0700

    file creation and classes assigned.

---
+++ Modles/Car.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace Dealership.Models
+{
+    public class Car
+    {
+        private string _makeModel;
+
+        public string MakeModel
+        {
+            get
+            {
+                return "That's a big secret. You'll have to visit Lonny's to find out about or special deals!";
+            }
+            set
+            {
+                _makeModel = value;
+            }
+        }
+        private int _price;
+        private int _miles;
+
+        public Car(string makeModel, int price, int miles)
+        {
+            _makeModel = makeModel;
+            _price = price;
+            _miles = miles;
+        }
+
+        public string GetMakeModel()
+        {
+            return _makeModel;
+        }
+
+        public int GetPrice()
+        {
+            return _price;
+        }
+
+        public int GetMiles()
+        {
+            return _miles;
+        }
+
+        public bool WorthBuying(int maxPrice)
+        {
+            return (_price <= maxPrice);
+        }
+
+        public void SetPrice(int newPrice)
+        {
+            _price = newPrice;
+        }
+
+        public static string MakeSound(string sound)
+        {
+            return "Our car sounds like " + sound;
+        }
+    }
+}
